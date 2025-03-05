a63155743a3d236c7405fec4b1480454d627fd6c
mart1n <20109376+mart1n-xyz@users.noreply.github.com>
2025-03-05 12:51:05 +0100

    minor updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,18 @@
       <p class="mb-4">
         Connect your wallet and explore what EIP-7702 can do for you. We've set up the UI for Holesky and Sepolia testnets, so you can experiment freely!
       </p>
-      
+      <div class="mt-4 bg-amber-50 px-3 py-2 rounded-md inline-flex items-start w-full">
+        <div class="flex-shrink-0 mt-0.5">
+          <svg class="h-4 w-4 text-amber-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="ml-2 flex-grow">
+          <span class="text-xs text-amber-700">
+            At the time of writing, this app does not work on Sepolia (Pectra hardfork bugs) and there is no support by any browser wallet yet. EIP-7702 also requires compatible RPC providers; this app uses Alchemy. Therefore, use this with a private key on Holesky for now.
+          </span>
+        </div>
+      </div>
       <div class="mt-4 space-y-4 mb-6">
         <div class=" border-gray-200 rounded-lg p-4 hover:bg-gray-50 transition-colors">
           <h3 class="text-lg font-medium text-gray-800 mb-2">👉 Try This Example: Batch Transfer</h3>
